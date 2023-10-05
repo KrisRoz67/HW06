42d404f7c1a2afe6a3a3db23c9a7758c97b66c12
Kristina Rozanova <zolina.kristina@gmail.com>
2023-10-05 08:57:30 +0300

    first commit

---
+++ src/IDCodeValidator.java
@@ -0,0 +1,163 @@
+import java.util.*;
+
+/**
+ * Класс для проверки эстонских личных кодов (ID code).
+ */
+public class IDCodeValidator {
+    /**
+     * Проверяет, является ли данный ID code допустимым.
+     * Использует все остальные методы для проверки
+     *
+     * @param idCode строка, представляющая ID code
+     * @return {@code true}, если ID code допустим, иначе {@code false}
+     */
+    public static boolean isCorrect(String idCode) {
+        if (idCode.length() == 11) {
+            System.out.println("The length of your ID code is right");
+        }
+            System.out.println("The length doesn't match the rules");
+            return false;
+    }
+    /**
+     * Проверяет корректность кода пола в ID code.
+     *
+     * @param genderNumber целое число, представляющее код пола
+     * @return {@code true}, если код пола корректен, иначе {@code false}
+     */
+    public static boolean isGenderNumberCorrect(int genderNumber){
+        return Gender.isGenderNumberCorrect(genderNumber);
+    }
+
+
+    /**
+     * Проверяет корректность номера месяца рождения в ID code.
+     *
+     * @param dayNumber целое число, представляющее номер месяца
+     * @return {@code true}, если номер дня корректен, иначе {@code false}
+     */
+    public static boolean isDayNumberCorrect(int dayNumber) {
+        return (dayNumber >0 && dayNumber <32);
+    }
+    /**
+     * Проверяет корректность номера месяца рождения в ID code.
+     *
+     * @param monthNumber целое число, представляющее номер месяца
+     * @return {@code true}, если номер месяца корректен, иначе {@code false}
+     */
+    public static boolean isMonthNumberCorrect(int monthNumber) {
+        return (monthNumber > 0 && monthNumber <13);
+    }
+
+
+    /**
+     * Проверяет корректность даты рождения в ID code.
+     * @param yearNumber  целое число, представляющее год рождения (1955, 1999, 2013)
+     * @param monthNumber целое число, представляющее номер месяца (1, 8)
+     * @param dayNumber   целое число, представляющее номер дня (15, 31)
+     * @return {@code true}, если номер дня корректен, иначе {@code false}
+     */
+    public static boolean isBirthDateCorrect(int yearNumber, int monthNumber, int dayNumber) {
+
+        HashMap<Integer, Integer> monthToDays = new HashMap<>();
+        monthToDays.put(1, 31);
+        monthToDays.put(2, 28);
+        monthToDays.put(3, 31);
+        monthToDays.put(4, 30);
+        monthToDays.put(5, 31);
+        monthToDays.put(6, 30);
+        monthToDays.put(7, 31);
+        monthToDays.put(8, 31);
+        monthToDays.put(9, 30);
+        monthToDays.put(10, 31);
+        monthToDays.put(11, 30);
+        monthToDays.put(12, 31);
+        if (monthNumber==2 && isLeapYear(yearNumber)){
+            return (dayNumber <= monthToDays.get(monthNumber)+1);
+        }else {
+            return  (dayNumber <= monthToDays.get(monthNumber));
+            }
+    }
+
+    /**
+     * Проверяет, является ли данный год високосным.
+     *
+     * @param yearNumber целое число, представляющее год
+     * @return {@code true}, если год високосный, иначе {@code false}
+     */
+    public static boolean isLeapYear(int yearNumber) {
+        return (yearNumber%4==0);
+    }
+
+    /**
+     * Проверяет корректность контрольной суммы в ID code.
+     *
+     * @param idCode строка, представляющая ID code
+     * @return {@code true}, если контрольная сумма корректна, иначе {@code false}
+     */
+    public static boolean checkControlNumber(String idCode) {
+        int result= 0;
+        List<Integer> listOfIdCodeNumbers = new ArrayList<>();
+        for (int i =0 ; i <idCode.length() ; i++){
+            String n = String.valueOf(idCode.charAt(i));
+            listOfIdCodeNumbers.add(Integer.parseInt(n));
+        }
+        List <Integer> x = List.of(1,2,3,4,5,6,7,8,9,1);
+        for (int i =0 ; i< listOfIdCodeNumbers.size()-1;i++){
+            result = result + (listOfIdCodeNumbers.get(i)*x.get(i));
+        }
+         int ostatok = result/11;
+         int ostatok2 = Math.abs(ostatok*11-result);
+
+        System.out.println("Is code"+  listOfIdCodeNumbers.get(listOfIdCodeNumbers.size()-1) );
+        System.out.println("ostatok" + ostatok2);
+
+        return (ostatok2 == listOfIdCodeNumbers.get(listOfIdCodeNumbers.size()-1) );
+    }
+
+    /**
+     * Метод, для получения информаций из ИД кода
+     * Используйте данный формат - This is a (gender) born on (DD.MM.YYYY).
+     * Gender в качестве ENUM
+     *
+     * @param idCode строка, представляющая ID code
+     * @return Строка, в которой написан пол и дата рождения человека
+     */
+    public static String getInformation(String idCode) {
+        PersonalCode personalCode = PersonalCode.getInformationFromIdCode(idCode);
+        int year = personalCode.year;
+        int genderNum = personalCode.genderNumber;
+        int month = personalCode.month;
+        int day = personalCode.year;
+        int fullYear = getFullYear(genderNum,year);
+        String gender = getGender(genderNum).gender;
+        return  String.format("This is a (%s) born on (%s.%s.%s)", gender,day,month,fullYear);
+    }
+
+    /**
+     * Верните пол человека
+     *
+     * @param genderNumber The gender number from the ID code.
+     * @return The gender as a string ("male" or "female").
+     */
+    public static Gender getGender(int genderNumber) {
+        return Gender.getGender(genderNumber);
+    }
+
+    /**
+     * Верните год, когда человек родился (1955, 2014)
+     *
+     * @param genderNumber The gender number from the ID code.
+     * @param year         The two last digits of the birth year from the ID code.
+     * @return The full 4-digit birth year.
+     */
+    public static int getFullYear(int genderNumber, int year) {
+        return switch (genderNumber) {
+            case 1, 2 -> 18 + year;
+            case 3, 4 -> 19 + year;
+            case 5, 6 -> 20 + year;
+            default -> throw new RuntimeException("Unknown gender");
+        };
+
+    }
+
+}
